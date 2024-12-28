c029ec6fd99297f7e5caaaf74ba51195176a38c1
RodrigoUma04 <151837337+RodrigoUma04@users.noreply.github.com>
2024-12-29 00:12:13 +0100

    **Temmporary** Trying to check collision by using a collisionmanager.

---
+++ ProjectGame/Core/CollisionManager.cs
@@ -0,0 +1,41 @@
+﻿using MonoGame.Extended.Tiled;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGame.Core
+{
+    class CollisionManager
+    {
+        private List<ICollidable> collidables;
+        private TiledMapLayer _collisionLayer;
+        private const int tileWidth = 32;
+        private const int tileHeight = 32;
+
+        public CollisionManager(TiledMapLayer collisionLayer)
+        {
+            collidables = new List<ICollidable>();
+            _collisionLayer = collisionLayer;
+        }
+
+        public void RegisterObject(ICollidable obj)
+        {
+            collidables.Add(obj);
+        }
+
+        public void UnregisterObject(ICollidable obj)
+        {
+            collidables.Remove(obj);
+        }
+
+        public void Update()
+        {
+            foreach(var coll in collidables)
+            {
+                /// Check for collision and call the OnCollision method
+            }
+        }
+    }
+}
---
+++ ProjectGame/Core/ICollidable.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGame.Core
+{
+    interface ICollidable
+    {
+        void OnCollision();
+    }
+}
